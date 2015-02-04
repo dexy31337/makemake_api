c33d5de9ce70b5c0a24055dc29f324a8e9dfb93a
dexy31337 <qdvvhZ37a>
2015-02-05 00:45:44 +0300

    Настройка маршрутов

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 MakemakeApi::Application.routes.draw do
-
+  #Определение API
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' },
+                  path : '/'
+                  do
+  end
 end
