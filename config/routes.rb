a18ce1623d4767750d6b064a55a1cf6b818bb7ba
dexy31337 <qdvvhZ37a>
2015-02-05 01:23:46 +0300

    API versioning setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+require 'api_contraints'
+
 MakemakeApi::Application.routes.draw do
   #Определение API
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
     end
   end
 end
