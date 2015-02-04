c90381b7dfdf0756ec7ec2b0217b74527580aa13
dexy31337 <qdvvhZ37a>
2015-02-05 00:32:17 +0300

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MakemakeApi::Application.config.secret_key_base = 'b5895d83f474bc4dbd7abe27dfd03d8ff0717acc6357d777db4ab35832fff6603493d466901b2463c1fa39316d7277116bf4e6dd852121698394c79d7380517a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MakemakeApi::Application.config.session_store :cookie_store, key: '_makemake_api_session'
